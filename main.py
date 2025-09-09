6600a1f409314d4ba823e6304e6437493b72d9de
esha kunder <eshakundercs2004@gmail.com>
2025-09-09 23:55:11 +0530

    data validation added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
-
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -19,3 +19,14 @@ except Exception as e:
     logger.exception(e)
     raise e
     
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -7,3 +7,9 @@ class DataIngestionConfig: # check 01_data_ingestion.ipynb
     source_URL:str
     local_data_file:Path
     unzip_dir:Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
